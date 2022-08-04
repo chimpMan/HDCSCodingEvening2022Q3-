bb4ff463a591f1c95c3fe2239a4545c3d3c749a1
chimpMan <98945839+chimpMan@users.noreply.github.com>
2022-08-04 20:58:50 +0300

    Update CodeChallenge.py

--- CodeChallenge.py
+++ CodeChallenge.py
@@ -1,16 +1,29 @@
 # getting the user input
-my_Input = input("Enter a string thats at least 3 characters long ")
-if(len(my_Input) < 3):
+
+my_input = input("Enter a string thats at least 3 characters long ")
+
+if(len(my_input) < 3):
     print("your string is less than 3 char long")
 
 middle_char = ""
+
+
+def main():
+
+    # creating the array to store my output
+    my_output = [string_length(), first_character(), last_character(),
+                 middle_characters(), my_index()]
+
+    # printing the output
+    print(my_output)
+    return my_output
 
 # function to get the length of a string
 
 
 def string_length():
 
-    lengths = len(my_Input)
+    lengths = len(my_input)
     # print(length_of_String)
     return lengths
 
@@ -18,7 +31,7 @@ def string_length():
 
 
 def first_character():
-    first_char = my_Input[0]
+    first_char = my_input[0]
     # print (first_char)
     return first_char
 
@@ -27,7 +40,7 @@ def first_character():
 
 def last_character():
 
-    last_char = my_Input[string_length()-1]
+    last_char = my_input[string_length()-1]
     # print (last_char)
     return last_char
 
@@ -38,30 +51,26 @@ def last_character():
 def middle_characters():
     mid = int((string_length()/2))
     if (string_length() % 2) == 0:
-        middle_char = my_Input[int(mid)-1, int(mid)]
+        middle_char = my_input[mid-1:mid+1]
     else:
-        middle_char = my_Input[int(mid)]
+        middle_char = my_input[int(mid)]
     return middle_char
 
 # function to get the index of  repeating second char in a string
 
 
 def my_index():
-    i = 2
+    i = 1
     ind_positon = ''
-    if (my_Input[i] == my_Input[1]):
-        ind_positon = f'@ index {i}'
+    if (i != string_length()-1):
         i = i+1
-    else:
-        ind_positon = 'not found'
+        print(i)
+        if (my_input[i] == my_input[1]):
+            ind_positon = f'@ index {i}'
+        else:
+            ind_positon = 'not found'
 
-    print(ind_positon)
     return ind_positon
 
 
-# creating the array to store my output
-myOutput = [string_length(), first_character(), last_character(),
-            middle_characters(), my_index()]
-
-# printing the output
-print(myOutput)
+main()
